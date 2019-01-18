92adc32eb0e49489b4357a4d50b64206a0c970e3
lee <lee@lee-PC>
2019-01-18 17:04:27 +0900

    t

--- src/main/java/controller/AbstractController.java
+++ src/main/java/controller/AbstractController.java
@@ -5,8 +5,6 @@ import http.HttpRequest;
 import http.HttpResponse;
 
 public abstract class AbstractController implements Controller {
-	
-	
     @Override
     public void service(HttpRequest request, HttpResponse response) {
         HttpMethod method = request.getMethod();
